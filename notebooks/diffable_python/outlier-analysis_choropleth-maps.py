93a8aaa813159d7c7ad9712b9068b94d6057ffc6
Lisa Hopcroft <54442530+LisaHopcroft@users.noreply.github.com>
2022-11-04 09:18:43 +0000

    feat: replacing old notebooks with new notebooks and outputs for publication

--- notebooks/diffable_python/outlier-analysis_choropleth-maps.py
+++ notebooks/diffable_python/outlier-analysis_choropleth-maps.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -12,9 +13,16 @@
 #     display_name: Python 3
 #     language: python
 #     name: python3
+#   vscode:
+#     interpreter:
+#       hash: de1343822d6e7d7aeea8796be9d48304b0fa3610166e8740495ec86b33c71a9e
 # ---
 
 # ## Antipsychotic outlier prescribing
+
+# + trusted=true
+### LH: modified from CCG_maps.ipynb'
+# -
 
 # This notebook identifies practices prescribing promazine and pericyazine and plots the proportions per CCG on a map.
 
@@ -76,10 +84,114 @@ ORDER BY
 anti_psy = bq.cached_read(sql, csv_path=os.path.join("..","data","anti_psy_df.csv"))
 
 # + trusted=true
+national_results = anti_psy.sum()
+national_pericyazine = 100 * (national_results['total_pericyazine']/national_results['total_antipsy'])
+national_promazine = 100 * (national_results['total_promazine']/national_results['total_antipsy'])
+
+print( f"The prescription rate for pericyazine nationally is {national_pericyazine.round(1)} %" )
+print( f"The prescription rate for promazine nationally is {national_promazine.round(1)} %" )
+
+
+# + trusted=true
 ccg_anti_psy = anti_psy.groupby(['pct', 'ccg_name']).sum().reset_index()
 ccg_anti_psy['promazine_per_1000_items'] = 1000* (ccg_anti_psy['total_promazine']/ccg_anti_psy['total_antipsy'])
 ccg_anti_psy['pericyazine_per_1000_items'] = 1000* (ccg_anti_psy['total_pericyazine']/ccg_anti_psy['total_antipsy'])
 ccg_anti_psy.head(5)
+
+# + trusted=true
+### Read in the geographic data that we need
+data_dir = os.path.join("..","data")
+cartogram_suffix = ""
+map_file = os.path.join(data_dir,"ccgs{}_{}.json").format(cartogram_suffix, '2018')
+ccgs = gpd.read_file(map_file)
+# Normalise names to match `ccg_fo_map` format (above)
+ccgs["name"] = ccgs["name"].str.upper()
+ccgs = ccgs.set_index("name")
+# Remove ones without geometry - these are (usually) federations
+# rather than individual CCGs
+ccgs = ccgs[~ccgs["geometry"].isnull()]
+
+map_name = os.path.join(data_dir, "ccg_for_map.csv")
+names = pd.read_csv(map_name)
+
+
+# + trusted=true
+### Identify the data that we need
+df = ccg_anti_psy
+df = df.merge(names[["code", "name", "region"]], left_on="pct", right_on="code")
+df = df.set_index("name")
+df.head()
+
+### Merge the geographic information with the data
+gdf = ccgs.join(df, rsuffix="_orig")
+
+
+# + trusted=true
+region_rates = gdf.groupby(['region']).sum().reset_index()
+region_rates['promazine_per_1000_items'] = 1000 * (region_rates['total_promazine']/region_rates['total_antipsy'])
+region_rates['pericyazine_per_1000_items'] = 1000 * (region_rates['total_pericyazine']/region_rates['total_antipsy'])
+
+pericyazine_results_by_region = region_rates[["total_pericyazine","total_antipsy","pericyazine_per_1000_items","region"]].set_index("region")
+pericyazine_results_by_region['pericyazine_per_1000_items'] = pericyazine_results_by_region['pericyazine_per_1000_items'].round(1)
+pericyazine_results_by_region['percentage'] = (100*(pericyazine_results_by_region['total_pericyazine']/pericyazine_results_by_region['total_antipsy'])).round(1)
+pericyazine_results_by_region.sort_values("pericyazine_per_1000_items", ascending=False, inplace=True)
+pericyazine_results_by_region
+
+# + trusted=true
+pericyazine_results_by_region.loc["Total"] = [
+    int(national_results.total_pericyazine),
+    int(national_results.total_antipsy),
+    ( 1000 * ( national_results.total_pericyazine / national_results.total_antipsy ) ).round(1),
+    ( 100 * ( national_results.total_pericyazine / national_results.total_antipsy ) ).round(1)
+]
+
+pericyazine_results_by_region['total_pericyazine'] = pd.to_numeric(pericyazine_results_by_region['total_pericyazine'], downcast='integer')
+pericyazine_results_by_region['total_antipsy'] = pd.to_numeric(pericyazine_results_by_region['total_antipsy'], downcast='integer')
+
+display( pericyazine_results_by_region )
+pericyazine_results_by_region.to_csv( '../output/pericyazine_results_by_region.csv' )
+
+
+
+# + trusted=true
+pericyazine_results_eastengland = gdf.loc[ gdf.region == "East of England" ][["total_pericyazine","total_antipsy","pericyazine_per_1000_items"]]
+pericyazine_results_eastengland['pericyazine_per_1000_items'] = pericyazine_results_eastengland['pericyazine_per_1000_items'].round(1)
+pericyazine_results_eastengland['percentage'] = (100*(pericyazine_results_eastengland['total_pericyazine']/pericyazine_results_eastengland['total_antipsy'])).round(1)
+pericyazine_results_eastengland.sort_values("pericyazine_per_1000_items",ascending=False, inplace=True)
+
+pericyazine_results_eastengland.to_csv( '../output/pericyazine_results_eastengland_region.csv' )
+display( pericyazine_results_eastengland )
+
+# + trusted=true
+
+promazine_results_by_region = region_rates[["total_promazine","total_antipsy","promazine_per_1000_items","region"]].set_index("region")
+promazine_results_by_region['promazine_per_1000_items'] = promazine_results_by_region['promazine_per_1000_items'].round(1)
+promazine_results_by_region['percentage'] = (100*(promazine_results_by_region['total_promazine']/promazine_results_by_region['total_antipsy'])).round(1)
+promazine_results_by_region.sort_values("promazine_per_1000_items", ascending=False, inplace=True)
+
+promazine_results_by_region.loc["Total"] = [
+    int(national_results.total_promazine),
+    int(national_results.total_antipsy),
+    ( 1000 * ( national_results.total_promazine / national_results.total_antipsy ) ).round(1),
+    ( 100 * ( national_results.total_promazine / national_results.total_antipsy ) ).round(1)
+]
+
+promazine_results_by_region['total_promazine'] = pd.to_numeric(promazine_results_by_region['total_promazine'], downcast='integer')
+promazine_results_by_region['total_antipsy'] = pd.to_numeric(promazine_results_by_region['total_antipsy'], downcast='integer')
+
+
+promazine_results_by_region.to_csv( '../output/promazine_results_by_region.csv' )
+display( promazine_results_by_region )
+
+
+# + trusted=true
+promazine_results_northwest = gdf.loc[ gdf.region == "North West" ][["total_promazine","total_antipsy","promazine_per_1000_items"]]
+promazine_results_northwest['promazine_per_1000_items'] = promazine_results_northwest['promazine_per_1000_items'].round(1)
+promazine_results_northwest['percentage'] = (100*(promazine_results_northwest['total_promazine']/promazine_results_northwest['total_antipsy'])).round(1)
+promazine_results_northwest.sort_values("promazine_per_1000_items",ascending=False, inplace=True)
+
+promazine_results_northwest.to_csv( '../output/promazine_results_northwest_region.csv' )
+display( promazine_results_northwest )
 
 
 # + trusted=true
